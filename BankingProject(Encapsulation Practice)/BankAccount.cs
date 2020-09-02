77529f143c8297c566007d1ea935a8785be4dd16
Bryce <ImperialEagle1998@gmail.com>
2020-09-02 11:45:20 -0500

    Add project files.

---
+++ BankingProject(Encapsulation Practice)/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingProject_Encapsulation_Practice_
+{
+    class BankAccount
+    {
+        private double balance = 0;
+
+
+        public void Deposit(double depositAmount)
+        {
+           balance += depositAmount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void Withdraw(double withdrawAmount)
+        {
+            balance -= withdrawAmount;
+        }
+    }
+}
---
+++ BankingProject(Encapsulation Practice)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankingProject_Encapsulation_Practice_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount Bryce = new BankAccount();
+
+            Bryce.Deposit(9400);
+            Console.WriteLine(Bryce.GetBalance());
+            Console.WriteLine("Bryce returns to withdraw a large sum of Cash to pay for a New Engineblock for his car, Certainly not happy.");
+            Bryce.Withdraw(7500);
+            Console.WriteLine(Bryce.GetBalance());
+          
+        }
+    }
+}
